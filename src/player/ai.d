bfe39b9fddb0caf9ee28da1488cb677e9511fc83
James Johnson <jjohnsonjj1251@gmail.com>
2017-08-23 10:51:53 -0400

    Starting complete rework of AI.

--- src/player/ai.d
+++ src/player/ai.d
@@ -10,8 +10,6 @@ import chess.movelist;
 import chess.position;
 import chess.sqattack;
 
-import core.time;
-
 import std.stdio;
 
 immutable Move emptyMove = Move(Piece.empty, Piece.empty, Piece.empty, Position(-1, -1), Position(-1, -1), Position(-1, -1), 0);
@@ -67,10 +65,6 @@ class AI : Player
 {
 private:
     Board board;
-    Move[64] PVarray;
-
-    Move[13][8][8] searchHistory;
-    Move[64][2] searchKillers;
 
     Position wKing;
     Position bKing;
@@ -112,27 +106,6 @@ public:
         return bMove;
     }
 
-    void reset()
-    {
-        for (int i = 0; i < 8; i++)
-            for (int j = 0; j < 8; j++)
-                for (int k = 0; k < 13; k++)
-                    searchHistory[i][j][k] = emptyMove;
-
-        for (int i = 0; i < 2; i++)
-            for (int j = 0; j < 64; j++)
-                searchKillers[i][j] = emptyMove;
-
-        ply = 0;
-
-        nodes = 0;
-
-        start = MonoTime.currTime;
-
-        fh = 0;
-        fhf = 0;
-    }
-
     bool validMove(Move m) const
     {
         MoveList ml = genMoves(board.SB);
@@ -148,160 +121,22 @@ public:
 
     int evalPosition() const
     {
-        int score = 0;
-
+        int score;
         for (int i = 0; i < 8; i++)
         {
             for (int j = 0; j < 8; j++)
             {
-                score += material[board[i, j]];
-
-                switch (board[i, j])
-                {
-                    case Piece.empty:
-                        break;
-                    case Piece.pawn:
-                        score -= PawnTable[i][j];
-                        break;
-                    case Piece.rook:
-                        score -= RookTable[i][j];
-                        break;
-                    case Piece.knight:
-                        score -= KnightTable[i][j];
-                        break;
-                    case Piece.bishop:
-                        score -= BishopTable[i][j];
-                        break;
-                    case Piece.Pawn:
-                        score += PawnTable[i][7-j];
-                        break;
-                    case Piece.Rook:
-                        score += RookTable[i][7-j];
-                        break;
-                    case Piece.Knight:
-                        score += KnightTable[i][7-j];
-                        break;
-                    case Piece.Bishop:
-                        score += BishopTable[i][7-j];
-                        break;
-                    default:
-                        break;
-                }
+                score += material[board[i][j]];
             }
         }
-
-        if (board.turn)
-            return -score;
-        else
-            return score;
-    }
-
-    void clearPVArray()
-    {
-        for (int i = 0; i < 64; i++)
-            PVarray[i] = emptyMove;
+        return score;
     }
 
     void searchPosition()
     {
-        Move bestMove = emptyMove;
-        int bestScore = -inf;
-        int currentDepth = void;
-        int pvNum = 0;
-        reset();
-
-        for (currentDepth = 1; currentDepth < 5; currentDepth++)
-        {
-            clearPVArray();
-
-            bestScore = alphaBeta(-inf, inf, currentDepth);
-
-            bestMove = PVarray[0];
-
-            writef("depth:%s score:%s nodes:%s ",
-                    currentDepth, bestScore, nodes);
-
-            for (int i = 0; PVarray[i] != emptyMove; i++)
-                writef(" %s", PVarray[i]);
-            write("\n");
-        }
-
-        bMove = bestMove;
-    }
-
-    int quiescence(int alpha, int beta)
-    {
-        return 0;
     }
 
     int alphaBeta(int alpha, int beta, int depth)
     {
-        assert(depth >= 0);
-        if (depth == 0)
-        {
-            nodes++;
-            return evalPosition();
-        }
-
-        nodes++;
-
-        if (board.repeatedBoard() || board.checkEnd())
-        {
-            return 0;
-        }
-
-        if (ply >= 64)
-            return evalPosition();
-
-        MoveList ml;
-        ml = genMoves(board.SB);
-
-        int legal = cast(int)(ml.length);
-        int oldAlpha = alpha;
-        Move bestMove = emptyMove;
-        int score = -inf;
-
-        foreach (m; ml.moves)
-        {
-            board.makeMove(m);
-            ply++;
-            score = -alphaBeta(-beta, -alpha, depth-1);
-            board.unMakeMove();
-            ply--;
-
-            if (score > alpha)
-            {
-                if (score >= beta)
-                {
-                    return beta;
-                }
-                alpha = score;
-                bestMove = m;
-            }
-
-        }
-
-        debug
-        {
-            import std.stdio;
-            writefln("Move: %s\nScore: %s\n", bestMove, score);
-        }
-
-        if (legal == 0)
-        {
-            if (board.turn && squareAttacked(bKing, board.SB, WHITE))
-                return -mate + ply;
-            else if (squareAttacked(wKing, board.SB, BLACK))
-                return -mate + ply;
-            else
-                return 0;
-        }
-
-        if (alpha != oldAlpha)
-        {
-            PVarray[ply] = bestMove;
-        }
-
-        return alpha;
     }
 }
